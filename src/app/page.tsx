b530ee6d55b576353364c2a0069af6c854c5bc15
George Satellite <lunaerxs@gmail.com>
2023-07-17 16:40:49 +0300

    remove titles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,10 @@
 export default function IndexPage() {
 	return (
-		<>
-			<Container>
-				<header className='pb-12 pt-16 md:py-20'>
-					<h1 className='relative -left-0.5 pb-2 text-5xl font-bold md:-left-1 md:text-7xl'>Simple Synth</h1>
-					<p className='text-base md:text-xl'>Simple synthesizer built with Elementary Audio.</p>
-				</header>
-			</Container>
-			<Container>
-				<div className='pb-16 md:pb-20'>
-					Hello
-				</div>
-			</Container>
-		</>
+		<Container>
+			<div className='py-16 md:py-20'>
+				Hello
+			</div>
+		</Container>
 	);
 }
 
