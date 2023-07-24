7a84a55e8ba672f4b9a6db592612acb36f304e1c
satelllte <lunaerxs@gmail.com>
2023-07-24 19:05:00 +0300

    Prettier (#11)

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -1,7 +1,8 @@
 /**
  * Clamps "x" value in [min..max] range
  */
-export const clamp = (x: number, min: number, max: number): number => Math.max(min, Math.min(max, x));
+export const clamp = (x: number, min: number, max: number): number =>
+  Math.max(min, Math.min(max, x));
 
 /**
  * Clamps "x" value in [0..1] range
@@ -11,21 +12,25 @@ export const clamp01 = (x: number): number => clamp(x, 0, 1);
 /**
  * Maps "x" value in [0..1] range onto [min..max] range linearly
  */
-export const mapFrom01Range = (x: number, min: number, max: number): number => ((max - min) * x) + min;
+export const mapFrom01Range = (x: number, min: number, max: number): number =>
+  (max - min) * x + min;
 
 /**
  * Maps "x" value in [min..max] range onto [0..1] range linearly
  */
-export const mapTo01Range = (x: number, min: number, max: number): number => (x - min) / (max - min);
+export const mapTo01Range = (x: number, min: number, max: number): number =>
+  (x - min) / (max - min);
 
 /**
  * Maps MIDI index to frequency in Hz. A4 MIDI note's index is 69
  */
-export const mapMidiToHz = (midiIndex: number): number => 440 * (2 ** ((midiIndex - 69) / 12));
+export const mapMidiToHz = (midiIndex: number): number =>
+  440 * 2 ** ((midiIndex - 69) / 12);
 
 /**
  * Gets MIDI note index by (note, octave) pair.
  * "note" - number from 0 to 11, defining a note from "C" to "B" (absolute, without specifying octave).
  * "octave" - number of octave from 1 to 9.
  */
-export const getMidiIndex = (note: number, octave: number): number => 12 + (12 * octave) + note;
+export const getMidiIndex = (note: number, octave: number): number =>
+  12 + 12 * octave + note;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,7 @@
 import type {Config} from 'tailwindcss';
 
 export default {
-	content: [
-		'./src/**/*.{ts,tsx}',
-	],
-	theme: {
-	},
-	plugins: [],
+  content: ['./src/**/*.{ts,tsx}'],
+  theme: {},
+  plugins: [],
 } satisfies Config;
