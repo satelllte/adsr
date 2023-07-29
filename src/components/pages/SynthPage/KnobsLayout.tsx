657ddb685d180c2bfa522a752af69e67bc3deffd
George Satellite <lunaerxs@gmail.com>
2023-07-30 00:23:25 +0300

    KnobsLayout: tweak gaps

--- src/components/pages/SynthPage/KnobsLayout.tsx
+++ src/components/pages/SynthPage/KnobsLayout.tsx
@@ -10,7 +10,7 @@ type KnobsLayoutProps = {
 
 export function KnobsLayout({children}: KnobsLayoutProps) {
   return (
-    <div className='grid grid-cols-2 grid-rows-3 gap-x-2 gap-y-3 sm:grid-cols-6 sm:grid-rows-none sm:gap-x-2 sm:gap-y-0'>
+    <div className='grid grid-cols-2 grid-rows-3 gap-x-2 sm:grid-cols-6 sm:grid-rows-none sm:gap-x-2 sm:gap-y-0'>
       <div className='col-span-2 flex justify-center sm:justify-start'>
         {children[0]}
       </div>
