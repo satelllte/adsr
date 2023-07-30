3b4f54571e13f2a8a615a4d56a704a47d7dc922b
George Satellite <lunaerxs@gmail.com>
2023-07-30 21:28:47 +0300

    InteractionArea: consistent UI

--- src/components/pages/SynthPage/SynthPageLayout.tsx
+++ src/components/pages/SynthPage/SynthPageLayout.tsx
@@ -5,8 +5,12 @@ type SynthPageLayoutProps = {
 export function SynthPageLayout({children}: SynthPageLayoutProps) {
   return (
     <div className='flex min-h-screen flex-col items-center justify-center'>
-      <div className='max-w-full px-4 pt-8'>{children[0]}</div>
-      <div className='max-w-full px-4 pt-16'>{children[1]}</div>
+      <div className='flex w-full items-center justify-center px-4 pt-8'>
+        {children[0]}
+      </div>
+      <div className='flex w-full items-center justify-center px-4 pt-16'>
+        {children[1]}
+      </div>
     </div>
   );
 }
