cf1e2b2317a465addeb3788a0b21a66beaca8798
George Satellite <lunaerxs@gmail.com>
2023-08-04 16:15:30 +0300

    gainToDecibels: more tests

--- src/utils/math/decibels.test.ts
+++ src/utils/math/decibels.test.ts
@@ -10,5 +10,10 @@ describe('gainToDecibels', () => {
     expect(gainToDecibels(0.25)).toBeCloseTo(-12.04);
     expect(gainToDecibels(0.125)).toBeCloseTo(-18.06);
     expect(gainToDecibels(0.0625)).toBeCloseTo(-24.08);
+    expect(gainToDecibels(0.03125)).toBeCloseTo(-30.1);
+    expect(gainToDecibels(0.015625)).toBeCloseTo(-36.12);
+    expect(gainToDecibels(0.0078125)).toBeCloseTo(-42.14);
+    expect(gainToDecibels(0.00390625)).toBeCloseTo(-48.16);
+    expect(gainToDecibels(0.001953125)).toBeCloseTo(-54.185);
   });
 });
