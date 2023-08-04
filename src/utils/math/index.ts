74c671162348f3b485f15e86366558ed679e101a
George Satellite <lunaerxs@gmail.com>
2023-08-04 16:15:22 +0300

    export gainToDecibels function

--- src/utils/math/index.ts
+++ src/utils/math/index.ts
@@ -1,4 +1,5 @@
 export {clamp, clamp01} from './clamp';
+export {gainToDecibels} from './decibels';
 export {NormalisableRange} from './NormalisableRange';
 export {mapFrom01Linear, mapTo01Linear} from './map01Linear';
 export {round} from './round';
