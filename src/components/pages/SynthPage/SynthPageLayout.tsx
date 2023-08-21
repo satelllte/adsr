7e7c160fbf5565e39944b46f13259285c6647f38
George Satellite <lunaerxs@gmail.com>
2023-08-21 20:31:00 +0300

    SynthPageLayout: center topPanel on mobile

--- src/components/pages/SynthPage/SynthPageLayout.tsx
+++ src/components/pages/SynthPage/SynthPageLayout.tsx
@@ -6,7 +6,9 @@ type SynthPageLayoutProps = {
 export function SynthPageLayout({topPanel, children}: SynthPageLayoutProps) {
   return (
     <div className='relative flex min-h-screen flex-col items-center justify-center'>
-      <div className='absolute left-0 top-0'>{topPanel}</div>
+      <div className='absolute left-1/2 top-0 max-w-full -translate-x-1/2 sm:left-0 sm:translate-x-0'>
+        {topPanel}
+      </div>
       <div className='flex w-full items-center justify-center px-4 pt-8'>
         {children[0]}
       </div>
