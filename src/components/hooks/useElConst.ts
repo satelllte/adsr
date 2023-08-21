46dfa56e357425f221b231f2ab3ccc9af5b8caab
George Satellite <lunaerxs@gmail.com>
2023-08-21 17:30:28 +0300

    useElConst

---
+++ src/components/hooks/useElConst.ts
@@ -0,0 +1,15 @@
+import {el, type NodeRepr_t} from '@elemaudio/core';
+import {useRef} from 'react';
+
+/**
+ * A hook that provides Elementary Audio `el.const`
+ */
+export const useElConst = (key: string, initialValue: number) => {
+  const constRef = useRef<NodeRepr_t>(el.const({key, value: initialValue}));
+  return {
+    get: () => constRef.current,
+    update(value: number) {
+      constRef.current = el.const({key, value});
+    },
+  };
+};
