8d3c357d5050ab4b6ed337be844fdc553e7b5c20
George Satellite <lunaerxs@gmail.com>
2023-08-23 18:37:56 +0300

    consume vitest's "performance.now" mocking

--- src/utils/math/LinearSmoothedValueRealtime.test.ts
+++ src/utils/math/LinearSmoothedValueRealtime.test.ts
@@ -1,10 +1,16 @@
-import {describe, expect, it} from 'vitest';
-import {PerformanceNowMock} from './../testing/PerformanceNowMock';
+import {afterEach, beforeEach, describe, expect, it, vi} from 'vitest';
 import {LinearSmoothedValueRealtime} from './LinearSmoothedValueRealtime';
 
 describe('LinearSmoothedValueRealtime', () => {
+  beforeEach(() => {
+    vi.useFakeTimers();
+  });
+
+  afterEach(() => {
+    vi.useRealTimers();
+  });
+
   it('interpolates properly', () => {
-    const performanceNowMock = new PerformanceNowMock(1000);
     const lsvr = new LinearSmoothedValueRealtime({
       currentValue: 0,
       targetValue: 100,
@@ -13,23 +19,20 @@ describe('LinearSmoothedValueRealtime', () => {
 
     expect(lsvr.getCurrentValue()).toBe(0);
 
-    performanceNowMock.advanceTimeBy(250);
+    vi.advanceTimersByTime(250);
     expect(lsvr.getCurrentValue()).toBe(25);
 
-    performanceNowMock.advanceTimeBy(500);
+    vi.advanceTimersByTime(500);
     expect(lsvr.getCurrentValue()).toBe(81.25);
 
-    performanceNowMock.advanceTimeBy(250);
+    vi.advanceTimersByTime(250);
     expect(lsvr.getCurrentValue()).toBe(100); // Ramp finishes at this point.
 
-    performanceNowMock.advanceTimeBy(250);
+    vi.advanceTimersByTime(250);
     expect(lsvr.getCurrentValue()).toBe(100); // Going past the ramp duration should not change the value.
-
-    performanceNowMock.end();
   });
 
   it('reacts to target value changes', () => {
-    const performanceNowMock = new PerformanceNowMock(15000);
     const lsvr = new LinearSmoothedValueRealtime({
       currentValue: 0,
       targetValue: 100,
@@ -38,26 +41,23 @@ describe('LinearSmoothedValueRealtime', () => {
 
     expect(lsvr.getCurrentValue()).toBe(0);
 
-    performanceNowMock.advanceTimeBy(500);
+    vi.advanceTimersByTime(500);
     expect(lsvr.getCurrentValue()).toBe(50);
 
     lsvr.setTargetValue(200);
     expect(lsvr.getCurrentValue()).toBe(50); // Ramp restarts from this time now. So it needs to pass full duration to reach target.
 
-    performanceNowMock.advanceTimeBy(500);
+    vi.advanceTimersByTime(500);
     expect(lsvr.getCurrentValue()).toBe(125);
 
-    performanceNowMock.advanceTimeBy(500);
+    vi.advanceTimersByTime(500);
     expect(lsvr.getCurrentValue()).toBe(200); // Ramp finishes at this point.
 
-    performanceNowMock.advanceTimeBy(500);
+    vi.advanceTimersByTime(500);
     expect(lsvr.getCurrentValue()).toBe(200); // Going past the ramp duration should not change the value.
-
-    performanceNowMock.end();
   });
 
   it('reacts to current & target value changes', () => {
-    const performanceNowMock = new PerformanceNowMock(15000);
     const lsvr = new LinearSmoothedValueRealtime({
       currentValue: 0,
       targetValue: 100,
@@ -69,9 +69,7 @@ describe('LinearSmoothedValueRealtime', () => {
     lsvr.setCurrentAndTargetValue(50);
     expect(lsvr.getCurrentValue()).toBe(50); // No ramping will happen anymore because current value equals to target.
 
-    performanceNowMock.advanceTimeBy(500);
+    vi.advanceTimersByTime(500);
     expect(lsvr.getCurrentValue()).toBe(50); // Going past the ramp duration should not change the value.
-
-    performanceNowMock.end();
   });
 });
