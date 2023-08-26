7b2bfe5ec54379997a8577c4857cf9fa60eea1bc
Yehor <lunaerxs@gmail.com>
2023-08-26 15:29:01 +0300

    Skeleton (#49)

--- src/components/ui/Meter.tsx
+++ src/components/ui/Meter.tsx
@@ -1,15 +1,20 @@
 import clsx from 'clsx';
 import {forwardRef} from 'react';
+import {Skeleton} from './Skeleton';
 
 type CanvasNativeProps = React.ComponentProps<'canvas'>;
 type MeterProps = Omit<CanvasNativeProps, 'className'>;
 
-const baseClass = clsx('absolute h-full w-full bg-gray-3');
+const baseClass = clsx('absolute h-full w-full');
 
 export const Meter = forwardRef<HTMLCanvasElement, MeterProps>((props, ref) => (
-  <canvas ref={ref} className={baseClass} {...props} />
+  <canvas ref={ref} className={clsx(baseClass, 'bg-gray-3')} {...props} />
 ));
 
 export function MeterSkeleton() {
-  return <div className={clsx(baseClass, 'motion-safe:animate-pulse')} />;
+  return (
+    <div className={baseClass}>
+      <Skeleton />
+    </div>
+  );
 }
---
+++ src/components/ui/Skeleton.tsx
@@ -0,0 +1,7 @@
+/**
+ * Skeleton component with animation.
+ * Takes up the full width and height of its parent.
+ */
+export function Skeleton() {
+  return <div className='h-full w-full bg-gray-3 motion-safe:animate-pulse' />;
+}
