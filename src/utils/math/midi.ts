8285f6d9829f2aca7010b33cb64a255e2f7d56cc
George Satellite <lunaerxs@gmail.com>
2023-08-27 16:05:50 +0300

    [skip deploy] JUCE reference in comment

--- src/utils/math/midi.ts
+++ src/utils/math/midi.ts
@@ -1,5 +1,8 @@
 /**
  * Gets frequency in Hz for corresponding MIDI note.
+ *
+ * Same as JUCE's `MidiMessage::getMidiNoteInHertz`:
+ * https://github.com/juce-framework/JUCE/blob/master/modules/juce_audio_basics/midi/juce_MidiMessage.cpp#L1037-L1040
  */
 export function midiNoteToFreq(note: number) {
   return 440 * 2 ** ((note - 69) / 12);
